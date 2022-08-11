c0f636a0e7329d1919476567cdf2f65811356951
AlineCantalice <alinedecantalice@gmail.com>
2022-08-10 21:33:09 -0300

    FEAT: componentes header e posts page, falta os posts

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Main from "./Pages/MainPage";
 import hashtagPage from "./Pages/hashtagPage";
+import Header from './Layouts/Header'
+import Posts from "./Pages/PostsPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/post" element={<Posts />} />
         <Route path="/home" element={<Main />} />
         <Route path="/hashtag/:hashtag" element={<hashtagPage />} />
       </Routes>
